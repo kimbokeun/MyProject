2fe296e7071ba3425d8235b47fa731f628c297a9
Kim Bokeun <kimbokeunn@gmail.com>
2024-07-25 16:55:08 +0900

    파일 삭제

--- test.js
+++ test.js
@@ -5,7 +5,6 @@ const prompt = require('prompt-sync')({siglnt:true});
 
 let targetNumber = Math.floor(Math.random() * 100) + 1;
 let guess = null;
-let guess = null;
 
 while (guess !== targetNumber) {
     guess = parseInt(prompt("Guess a number between 1 and 100:"));
